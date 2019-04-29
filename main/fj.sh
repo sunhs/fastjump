cf0c180078833e097c22ab54a990653be8db3a5a
edwardzhu <edwardzhu@tencent.com>
2019-04-29 11:52:42 +0800

    mod sh

--- main/fj.sh
+++ main/fj.sh
@@ -1,7 +1,11 @@
 fj() {
-    local output="$(fastjump ${@})"
-    if [[ "$#" -eq 1 && "$1" != "-l" ]]
-    then cd "${output}"
-    else echo "${output}"
+    local output
+    output=$(fastjump $@)
+    code=$?
+    if [[ $# -eq 1 && $1 != "-l" && $code -eq 0 ]]
+    then cd $output
+    elif [[ $1 = "-l" || $code -ne 0 ]]
+    then echo $output
+    else echo "succeeded"
     fi
 }
