794c386ee690d23bb6afa3c10229d91b7fc54998
hyesunzhu <hyesunzhu@outlook.com>
2019-09-23 09:31:16 +0000

    add fjrc

--- main/fj.sh
+++ main/fj.sh
@@ -3,8 +3,7 @@ fj() {
     local argc
     local output
     local code
-    if [ $# -eq 0 ]
-    then
+    if [ $# -eq 0 ]; then
         args="~"
         argc=1
     else
@@ -13,10 +12,23 @@ fj() {
     fi
     output=$(fastjump $args)
     code=$?
-    if [[ $argc -eq 1 && $args != "-l" && $code -eq 0 ]]
-    then cd "$output"
-    elif [[ $1 = "-l" || $code -ne 0 ]]
-    then echo "$output"
-    else echo "succeeded"
+    if [[ $argc -eq 1 && $args != "-l" && $code -eq 0 ]]; then
+        cd "$output"
+    elif [[ $1 = "-l" || $code -ne 0 ]]; then
+        echo "$output"
+    else echo "Succeeded."
     fi
+}
+
+fjrc() {
+    if [ ! $# -eq 0 ]; then
+        echo "Wrong arguments. Use fjrc directly to remove the current dir from the DB."
+        return 1
+    fi
+    cnt=$(fj -l | grep $(pwd) | wc -l)
+    while [ ! $cnt -eq 0 ]; do
+        i=$(fj -l | grep $(pwd) | head -n 1 | awk '{print $1}')
+        fj -r $i
+        cnt=$(( $cnt - 1 ))
+    done
 }
